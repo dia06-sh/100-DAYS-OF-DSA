cb73220e42ba8eac3466cbf59b19f6d453c6bb76
dia06-sh <Dia.26300@stu.upes.ac.in>
2026-02-25 13:27:09 +0530

    Add files via upload

---
+++ day21.c
@@ -0,0 +1,31 @@
+int main() {
+    int n, i;
+    scanf("%d", &n);
+
+    struct Node *head = NULL, *newNode, *temp;
+
+    for(i = 0; i < n; i++) {
+        newNode = (struct Node*)malloc(sizeof(struct Node));
+        scanf("%d", &newNode->data);
+        newNode->next = NULL;
+
+        if(head == NULL) {
+            head = newNode;
+        } else {
+            temp = head;
+            while(temp->next != NULL) {
+                temp = temp->next;
+            }
+            temp->next = newNode;
+        }
+    }
+
+
+    temp = head;
+    while(temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+
+    return 0;
+}
